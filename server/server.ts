ba53b3a346c473989994399c63b76f3d77a5a74d
jimjones26 <jimjones26@gmail.com>
2016-12-26 13:12:29 -0500

    add nodemon, create first api call route

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,17 @@
+import * as express from 'express';
+import { Application } from 'express';
+
 import { findAllCourses } from './queries/findAllCourses';
 
-findAllCourses()
-  .then(results => console.log(JSON.stringify(results)));
+const app: Application = express();
+
+app.route('/api/courses').get((req, res) => {
+  findAllCourses()
+    .then(results => {
+      res.status(200).json(results);
+    });
+});
+
+app.listen(8090, () => {
+  console.log('server is running...');
+});
