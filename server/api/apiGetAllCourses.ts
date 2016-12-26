de158cdce7c72f563d29533c5242db33cee0f419
jimjones26 <jimjones26@gmail.com>
2016-12-26 18:18:15 -0500

    add some success handling

--- server/api/apiGetAllCourses.ts
+++ server/api/apiGetAllCourses.ts
@@ -3,12 +3,10 @@ import * as _ from 'lodash';
 
 import { findAllCourses } from '../queries/findAllCourses';
 import { onError } from './onError';
+import { onSuccess } from './onSuccess';
 
 export function apiGetAllCourses(req: Request, res: Response) {
   findAllCourses()
-    .then(() => { throw new Error('Error') })
-    .then(results => {
-      res.status(200).json({ results });
-    })
+    .then(_.partial(onSuccess, res))
     .catch(_.partial(onError, res, "Find All Courses Failed"));
 }
