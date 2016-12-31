616b94346ad91afe4a947f634011076d014d2213
jimjones26 <jimjones26@gmail.com>
2016-12-31 15:05:02 -0500

    ---

--- server/queries/findAllCourses.ts
+++ server/queries/findAllCourses.ts
@@ -1,7 +1,11 @@
+import { CourseSummary } from './../../shared/model/course-summary';
 import { CourseModel } from '../model/model';
+import { createCourseSummaries } from '../../shared/model/course-summary';
+import Bluebird = require('bluebird');
 
-export function findAllCourses() {
+export function findAllCourses(): Bluebird<CourseSummary[]> {
   return CourseModel.findAll({
     order: ['seqNo']
   })
+  .then(createCourseSummaries)
 }
